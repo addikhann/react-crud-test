92bb53671f76196e7981a09073020b7095fd2bb9
Adeel Khan <mr.addikhan@gmail.com>
2023-07-04 01:34:46 +0500

    First

---
+++ src/data/index.js
@@ -0,0 +1,36 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Addi',
+    lastName: 'khan',
+    email: 'addi@abc.com',
+    salary: '95000',
+    date: '2022-04-11',
+  },
+  {
+    id: 2,
+    firstName: 'Khan',
+    lastName: 'Sahab',
+    email: 'Khan@gmail.com',
+    salary: '80000',
+    date: '2022-04-17',
+  },
+  {
+    id: 3,
+    firstName: 'Rolf',
+    lastName: 'Kent',
+    email: 'Rolf@example.com',
+    salary: '79000',
+    date: '2022-05-01',
+  },
+  {
+    id: 4,
+    firstName: 'Kent',
+    lastName: 'Kent',
+    email: 'Rolf@example.com',
+    salary: '56000',
+    date: '2022-05-03',
+  }
+];
+
+export { employeesData };
